48eaaf9fe4cea82a5aa30cd3042ea32582763436
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-01-14 22:01:40 +0000

    minor edit to lab 3-3

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -15,8 +15,8 @@
 -- | 2019 | Houston Astros                | 107       |
 -- | 2004 | St. Louis Cardinals           | 105       |
 
-SELECT year, name, MAX(wins)
+SELECT year, MAX(wins)
 FROM teams
-WHERE year > 1960
+WHERE year >= 1960
 GROUP BY year
 ORDER BY MAX(wins) DESC;
