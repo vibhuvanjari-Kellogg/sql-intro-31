6c990957ec2efdaa0f6bd3cb31148de2f1b0a34f
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-01-14 22:30:43 +0000

    completed lab 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,8 @@
 -- Expected result:
 -- 2935
 
-
+SELECT players.id, players.first_name, players.last_name, SUM(stats.hits)
+FROM stats
+INNER JOIN players ON players.id = stats.player_id
+WHERE players.first_name = "Barry"
+AND players.last_name = "Bonds";
