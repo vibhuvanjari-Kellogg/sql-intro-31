633c17baa2055a5679edaeda11d0cfbf6f225486
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-01-21 05:03:20 +0000

    fisrt attempt lab 4-3

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -6,4 +6,10 @@
 -- | New York Mets | Pete       | Alonso    | 53        |
 -- +---------------+------------+-----------+-----------+
 
-
+SELECT teams.name, players.first_name, players.last_name, stats.home_runs
+FROM stats
+INNER JOIN teams ON teams.id = stats.team_id
+INNER JOIN players ON players.id = stats.player_id
+WHERE year = 2019
+ORDER BY home_runs DESC
+LIMIT 1;
